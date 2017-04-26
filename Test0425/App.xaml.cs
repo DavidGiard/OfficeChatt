899ca5da8c5de092c7562ec602b58ee9ca7e6357
DavidGiard <davidjgiard@yahoo.com>
2017-04-26 15:51:21 -0500

    Initial commit

---
+++ Test0425/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace Test0425
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new Test0425Page();
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ Test0425/Test0425Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Test0425
+{
+	public partial class Test0425Page : ContentPage
+	{
+		public Test0425Page()
+		{
+			InitializeComponent();
+		}
+	}
+}
